eb76da6964c44b69f515f2e1b2812ebf9401a319
lswlc33 <lswlc33@qq.com>
2023-07-25 21:23:54 +0800

    添加了文案，增加了向左的

--- bachong/js/myjs.js
+++ bachong/js/myjs.js
@@ -1,4 +1,26 @@
-function create_img() {
+function create_img_left() {
+    var img = document.createElement('img')
+    var left = window.innerWidth + 'px'
+    var top = Math.random() * (window.innerHeight - 280) + 'px'
+
+    img.src = "img/go_left.gif"
+    img.style.position = `fixed`;
+    img.style.left = left
+    img.style.transition = 'left 2s'
+    img.style.top = top
+
+    document.body.appendChild(img)
+    setTimeout(() => {
+        img.style.left = "-280px"
+    }, 100);
+
+    setTimeout(() => {
+        document.body.removeChild(img)
+    }, 3000);
+
+}
+
+function create_img_right() {
     var img = document.createElement('img')
     var left = window.innerWidth + 'px'
     var top = Math.random() * (window.innerHeight - 280) + 'px'
@@ -17,5 +39,15 @@ function create_img() {
     setTimeout(() => {
         document.body.removeChild(img)
     }, 3000);
+
+}
+
+function create_img() { 
+    var num = Math.random()
+    if (num >= 0.5) {
+        create_img_right()
+    } else { 
+        create_img_left()
+    }
 
 }
