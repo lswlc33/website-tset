1e6ccfe0fc7b4d41b8f67cc1b94875f15c6b61e3
lswlc33 <lswlc33@qq.com>
2023-08-01 14:12:10 +0800

    时钟功能实现

--- home/js/myjs.js
+++ home/js/myjs.js
@@ -15,3 +15,15 @@ function expand_close() {
     left.style.display = 'flex'
     right.style.display = 'none'
 }
+
+function change_time() {
+    var c_time = new Date()
+    var clock = document.getElementById('time_box')
+
+    c_time = c_time.getHours() + ":" + c_time.getMinutes() + ":" + c_time.getSeconds()
+    clock.innerHTML = c_time
+}
+
+setInterval(() => {
+    change_time()
+}, 1000);
