4da464d6cdd8799954069943d5e8a28e93745752
lswlc33 <lswlc33@qq.com>
2023-08-01 16:15:13 +0800

    时间格式优化

--- home/js/myjs.js
+++ home/js/myjs.js
@@ -17,10 +17,25 @@ function expand_close() {
 }
 
 function change_time() {
-    var c_time = new Date()
+    var date = new Date();
     var clock = document.getElementById('time_box')
 
-    c_time = c_time.getHours() + ":" + c_time.getMinutes() + ":" + c_time.getSeconds()
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+  
+    if (hours < 10) {
+      hours = "0" + hours;
+    }
+    if (minutes < 10) {
+      minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
+  
+    var c_time = hours + ":" + minutes + ":" + seconds;
+
     clock.innerHTML = c_time
 }
 
