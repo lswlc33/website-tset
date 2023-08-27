f69617aa8d32a0f498bec646a3953e8dc81bcc85
lswlc33 <lswlc33@qq.com>
2023-08-27 21:55:26 +0800

    添加侧边栏展开

--- cemiuiler/my.js
+++ cemiuiler/my.js
@@ -5,16 +5,23 @@ function change_active() {
     for (let floor = 0; floor < sidebar_items.length; floor++) {
         // 切换高亮
         const sidebar_item = sidebar_items[floor];
-        if (this == sidebar_item){
+        if (this == sidebar_item) {
             float_line.style.top = `${100 * floor}%`
             return
         }
     }
 }
 
+function change_sidebar_active() {
+    var sidebar_expend = document.querySelector('#sidebar_expend')
+    if (sidebar_expend.className == 'sidebar_expend') {
+        sidebar_expend.className = 'sidebar_expend_active'
+    } else {
+        sidebar_expend.className = 'sidebar_expend'
+    }
+}
+
 sidebar_items.forEach(sidebar_item => {
     sidebar_item.addEventListener('click', change_active)
+    sidebar_item.addEventListener('dblclick', change_sidebar_active)
 });
-
-
-
